97f792620ddd3c7ada08a6c28818b050b77a163c
Linus Unnebäck <linus@folkdatorn.se>
2019-11-27 18:55:27 +0000

    🎉 Improve TypeScript typings

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,14 @@
-import { StatelessComponent } from 'react'
+import { FC } from 'react'
 
-declare interface SpacerProps {
+declare interface Props {
+  /**
+   * The size of the spinner. Can be specified as any css-compatible length.
+   *
+   * e.g. `'100%'`, `'128px'`, `'1em'`
+   */
   size?: string
 }
 
-declare const IndeterminateSpinner: StatelessComponent<SpacerProps>
+declare const IndeterminateSpinner: FC<Props>
 
 export = IndeterminateSpinner
